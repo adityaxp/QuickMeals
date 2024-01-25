391ea6d651f84a111545d296ce2499fbea71a911
Aditya <adityabalsane10@gmail.com>
2024-01-25 17:52:44 +0530

    v0.5

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -3,8 +3,23 @@ import styled from "styled-components/native";
 import { Camera, CameraType } from "expo-camera";
 import { View } from "react-native";
 import { Text } from "../../../components/typography/text.component";
+import { Button } from "react-native-paper";
 
 const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
+
+const CameraButton = styled(Button).attrs({
+  mode: "contained",
+  icon: "camera",
+})`
+  position: absolute;
+  top: 525px;
+  left: 140px;
+`;
+
+const CameraContainer = styled.View`
   width: 100%;
   height: 100%;
 `;
@@ -12,6 +27,13 @@ const ProfileCamera = styled(Camera)`
 export const CameraScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+
+  const snap = async () => {
+    if (cameraRef && cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+    }
+  };
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
@@ -26,9 +48,12 @@ export const CameraScreen = () => {
     return <Text>No access to camera</Text>;
   }
   return (
-    <ProfileCamera
-      ref={(camera) => (cameraRef.current = camera)}
-      type={CameraType.front}
-    ></ProfileCamera>
+    <CameraContainer>
+      <ProfileCamera
+        ref={(camera) => (cameraRef.current = camera)}
+        type={CameraType.front}
+      ></ProfileCamera>
+      <CameraButton onPress={snap}>Snap</CameraButton>
+    </CameraContainer>
   );
 };
