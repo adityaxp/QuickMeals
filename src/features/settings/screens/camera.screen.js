a070c695da873cda4d5f492464fffa440bb32a67
Aditya <adityabalsane10@gmail.com>
2024-01-25 17:37:21 +0530

    v0.5

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,3 +1,34 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import styled from "styled-components/native";
+import { Camera, CameraType } from "expo-camera";
+import { View } from "react-native";
+import { Text } from "../../../components/typography/text.component";
 
-export const CameraScreen = () => null;
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef();
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={CameraType.front}
+    ></ProfileCamera>
+  );
+};
