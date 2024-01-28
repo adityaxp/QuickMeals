59f7f7fcadf86d44736104bd033926fe94492141
Aditya <adityabalsane10@gmail.com>
2024-01-28 21:44:24 +0530

    v0.6

--- functions/places/index.js
+++ functions/places/index.js
@@ -1,3 +1,12 @@
+const { mocks, addMockImage } = require("./mock");
+const url = require("url");
+
 module.exports.placesRequest = (request, response) => {
-  response.send("Places");
+  const { location } = url.parse(request.url, true).query;
+  const data = mocks[location];
+  if (data) {
+    data.results = data.results.map(addMockImage);
+  }
+
+  response.json(data);
 };
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,22 +1,9 @@
-import { mocks, mockImages } from "./mock";
 import camelize from "camelize";
 
-export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("not found");
-    }
-    resolve(mock);
-  });
-};
+export const restaurantsRequest = (location) => {};
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
-
     return {
       ...restaurant,
       address: restaurant.vicinity,
