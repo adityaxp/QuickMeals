7ee393b439d9838a429ba027df8f858f29dd3ba6
Aditya <adityabalsane10@gmail.com>
2024-01-30 22:30:32 +0530

    v0.8

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { AppNavigator } from "./app.navigator";
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
-import { View, Text } from "react-native";
 import { AccountNavigator } from "./account.navigator";
 import { NavigationContainer } from "@react-navigation/native";
 export const Navigation = () => {
